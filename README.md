2233b7d2ecb6724ba53e3b091bd898d1f5ca3c97
vivekjava <vivekjava96@gmail.com>
2020-05-04 11:09:30 +0530

    Go Microservice

---
+++ README.md
@@ -0,0 +1,9 @@
+# schedulemaster
+
+This Service only will trigger the scheduled task.
+
+## Steps or Tasks 
+
+1. scheduled tridder
+2. Retrieve all records based on the pagination
+3. 
---
+++ main.go
@@ -0,0 +1,27 @@
+/*
+    Ref : https://tutorialedge.net/golang/creating-restful-api-with-golang/
+
+*/
+
+
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Welcome to the HomePage!")
+    fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+    http.HandleFunc("/", homePage)
+    log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func main() {
+    handleRequests()
+}
